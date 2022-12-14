bd0fdffd8eafd370eb14a9a1ce09c8eac6cf3dad
Andre <asm13august95@gmail.com>
2022-12-14 17:36:01 -0600

    initial setup, Navbar and Sidebar work

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, Navbar, Profile } from '.';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/" component={Movies} />
+          <Route exact path="/profile/:id" component={Profile} />
+          <Route exact path="/movie/:id" component={MovieInformation} />
+          <Route exact path="/actors/:id" component={Actors} />
+
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
+export default App;
