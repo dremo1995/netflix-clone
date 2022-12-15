9e736bdc4dd5c19174a78bfe4eff741f43edb476
Andre <asm13august95@gmail.com>
2022-12-15 13:53:42 -0600

    worked on Movie display on main page

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '../MovieList/MovieList';
 
 const Movies = () => {
-  console.log('Movies');
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+      >
+        <CircularProgress color="secondary" />
+      </Box>
+    );
+  } if (!data.results.length) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        mt="20px"
+      >
+        <Typography variant="h5">No movies found <br /> Please Search for something else</Typography>
+      </Box>
+    );
+  } if (error) return 'An error occurred';
+
   return (
-    <div>Movies</div>
+    <div><MovieList movies={data} /></div>
   );
 };
 
